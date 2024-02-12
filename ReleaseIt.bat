f35a3fbcfddba58eaefb147318dcd6069a3feada
AlBuSoft <a.burkhardt@adg.de>
2024-02-12 15:54:53 +0100

    ReleaseIt.bat implementiert

---
+++ ReleaseIt.bat
@@ -0,0 +1,19 @@
+@echo off
+:: Copy binary files x64
+xcopy Release-x64\*.dll RELEASE\x64\ /y /v
+xcopy Release-x64\*.lib RELEASE\x64\ /y /v
+:: Copy binary files x86
+xcopy Release-Win32\*.dll RELEASE\x86\ /y /v
+xcopy Release-Win32\*.lib RELEASE\x86\ /y /v
+:: Copy header-files
+xcopy libs\ecm\ecm\*.h RELEASE\include\ecm\ /y /v /s
+xcopy libs\ecm\ecm\*.hpp RELEASE\include\ecm\ /y /v /s
+xcopy libs\ecm.algorithm\ecm\*.h RELEASE\include\ecm\ /y /v /s
+xcopy libs\ecm.algorithm\ecm\*.hpp RELEASE\include\ecm\ /y /v /s
+:: Pack released library
+"C:\Program Files\7-Zip\7z.exe" a -r -mx9 .\RELEASE\ecm_win32_x64 .\RELEASE\x64\
+"C:\Program Files\7-Zip\7z.exe" a -r -mx9 .\RELEASE\ecm_win32_x86 .\RELEASE\x86\
+"C:\Program Files\7-Zip\7z.exe" a -r -mx9 .\RELEASE\ecm_win32_x64 .\RELEASE\include\
+"C:\Program Files\7-Zip\7z.exe" a -r -mx9 .\RELEASE\ecm_win32_x86 .\RELEASE\include\
+::pause
+timeout -t 10
