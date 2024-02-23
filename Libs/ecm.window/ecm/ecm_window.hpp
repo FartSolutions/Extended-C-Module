9f5f3308ad32dfb9b8949e92793930d511a8f40c
AlBuSoft <a.burkhardt@adg.de>
2024-02-23 13:05:26 +0100

    Includepfade der Projekte und anderes aangepasst

--- Libs/ecm.window/ecm/ecm_window.hpp
+++ Libs/ecm.window/ecm/ecm_window.hpp
@@ -26,31 +26,15 @@
 */
 
 /*
- * \file ecm_types.hpp
+ * \file ecm_window.hpp
  *
- * \brief This header includes all the public types header.
+ * \brief This header includes all the public window header.
  */
 
 #pragma once
-#ifndef _ECM_TYPES_HPP_
-#define _ECM_TYPES_HPP_
+#ifndef _ECM_WINDOW_HPP_
+#define _ECM_WINDOW_HPP_
 
-#include "types/stdtypes.h"
-#include "types/color.h"
-#include "types/id.h"
 
-#include <assert.h>
 
-#define ECM_SUCCESS 0
-#define ECM_FAILED 1
-
-#define ECM_ASSERT(ex) assert(ex)
-#define ECM_NODISCARD [[nodiscard]]
-
-#if ECM_DEBUG
-#define ECM_DEBUGOP(op) op
-#else
-#define ECM_DEBUGOP(op)
-#endif
-
-#endif // !_ECM_TYPES_HPP_
+#endif // !_ECM_WINDOW_HPP_
--- Libs/ecm/ecm/ecm.hpp
+++ Libs/ecm/ecm/ecm.hpp
@@ -26,31 +26,17 @@
 */
 
 /*
- * \file ecm_types.hpp
+ * \file ecm.hpp
  *
- * \brief This header includes all the public types header.
+ * \brief This header includes all public ecm header.
  */
 
 #pragma once
-#ifndef _ECM_TYPES_HPP_
-#define _ECM_TYPES_HPP_
+#ifndef _ECM_HPP_
+#define _ECM_HPP_
 
-#include "types/stdtypes.h"
-#include "types/color.h"
-#include "types/id.h"
+#include <ecm/container/vector.hpp>
+#include <ecm/ecm_types.hpp>
+#include <ecm/ecm_window.hpp>
 
-#include <assert.h>
-
-#define ECM_SUCCESS 0
-#define ECM_FAILED 1
-
-#define ECM_ASSERT(ex) assert(ex)
-#define ECM_NODISCARD [[nodiscard]]
-
-#if ECM_DEBUG
-#define ECM_DEBUGOP(op) op
-#else
-#define ECM_DEBUGOP(op)
-#endif
-
-#endif // !_ECM_TYPES_HPP_
+#endif // !_ECM_HPP_
