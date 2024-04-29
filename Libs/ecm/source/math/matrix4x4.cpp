9657a7e55494c4751b5f0bbffef1fb4612fe57a1
Alexander <alexander.bu1@gmx.net>
2024-04-29 16:57:22 +0200

    Matrix4x4 multiplication and operatro improvements

--- Libs/ecm/source/math/matrix4x4.cpp
+++ Libs/ecm/source/math/matrix4x4.cpp
@@ -78,77 +78,68 @@ namespace ecm::math
 
 	constexpr Matrix4x4 operator+(const Matrix4x4& left, const Matrix4x4& right)
 	{
-		Matrix4x4 mat{};
-		mat.m00 = left.m00 + right.m00;
-		mat.m01 = left.m01 + right.m01;
-		mat.m02 = left.m02 + right.m02;
-		mat.m03 = left.m03 + right.m03;
-
-		mat.m10 = left.m10 + right.m10;
-		mat.m11 = left.m11 + right.m11;
-		mat.m12 = left.m12 + right.m12;
-		mat.m13 = left.m13 + right.m13;
-
-		mat.m20 = left.m20 + right.m20;
-		mat.m21 = left.m21 + right.m21;
-		mat.m22 = left.m22 + right.m22;
-		mat.m23 = left.m23 + right.m23;
-
-		mat.m30 = left.m30 + right.m30;
-		mat.m31 = left.m31 + right.m31;
-		mat.m32 = left.m32 + right.m32;
-		mat.m33 = left.m33 + right.m33;
-		return mat;
+		return Matrix4x4(
+			left.m00 + right.m00,
+			left.m01 + right.m01,
+			left.m02 + right.m02,
+			left.m03 + right.m03,
+			left.m10 + right.m10,
+			left.m11 + right.m11,
+			left.m12 + right.m12,
+			left.m13 + right.m13,
+			left.m20 + right.m20,
+			left.m21 + right.m21,
+			left.m22 + right.m22,
+			left.m23 + right.m23,
+			left.m30 + right.m30,
+			left.m31 + right.m31,
+			left.m32 + right.m32,
+			left.m33 + right.m33
+		);
 	}
 
 	constexpr Matrix4x4 operator-(const Matrix4x4& left, const Matrix4x4& right)
 	{
-		Matrix4x4 mat{};
-		mat.m00 = left.m00 - right.m00;
-		mat.m01 = left.m01 - right.m01;
-		mat.m02 = left.m02 - right.m02;
-		mat.m03 = left.m03 - right.m03;
-
-		mat.m10 = left.m10 - right.m10;
-		mat.m11 = left.m11 - right.m11;
-		mat.m12 = left.m12 - right.m12;
-		mat.m13 = left.m13 - right.m13;
-
-		mat.m20 = left.m20 - right.m20;
-		mat.m21 = left.m21 - right.m21;
-		mat.m22 = left.m22 - right.m22;
-		mat.m23 = left.m23 - right.m23;
-
-		mat.m30 = left.m30 - right.m30;
-		mat.m31 = left.m31 - right.m31;
-		mat.m32 = left.m32 - right.m32;
-		mat.m33 = left.m33 - right.m33;
-		return mat;
+		return Matrix4x4(
+			left.m00 - right.m00,
+			left.m01 - right.m01,
+			left.m02 - right.m02,
+			left.m03 - right.m03,
+			left.m10 - right.m10,
+			left.m11 - right.m11,
+			left.m12 - right.m12,
+			left.m13 - right.m13,
+			left.m20 - right.m20,
+			left.m21 - right.m21,
+			left.m22 - right.m22,
+			left.m23 - right.m23,
+			left.m30 - right.m30,
+			left.m31 - right.m31,
+			left.m32 - right.m32,
+			left.m33 - right.m33
+		);
 	}
 
 	constexpr Matrix4x4 operator*(const Matrix4x4& left, const Matrix4x4& right)
 	{
-		Matrix4x4 mat{};
-		mat.m00 = left.m00 * right.m00;
-		mat.m01 = left.m01 * right.m01;
-		mat.m02 = left.m02 * right.m02;
-		mat.m03 = left.m03 * right.m03;
-
-		mat.m10 = left.m10 * right.m10;
-		mat.m11 = left.m11 * right.m11;
-		mat.m12 = left.m12 * right.m12;
-		mat.m13 = left.m13 * right.m13;
-
-		mat.m20 = left.m20 * right.m20;
-		mat.m21 = left.m21 * right.m21;
-		mat.m22 = left.m22 * right.m22;
-		mat.m23 = left.m23 * right.m23;
-
-		mat.m30 = left.m30 * right.m30;
-		mat.m31 = left.m31 * right.m31;
-		mat.m32 = left.m32 * right.m32;
-		mat.m33 = left.m33 * right.m33;
-		return mat;
+		return Matrix4x4(
+			left.m00 * right.m00 + left.m01 * right.m10 + left.m02 * right.m20 + left.m03 * right.m30,
+			left.m00 * right.m01 + left.m01 * right.m11 + left.m02 * right.m21 + left.m03 * right.m31,
+			left.m00 * right.m02 + left.m01 * right.m12 + left.m02 * right.m22 + left.m03 * right.m32,
+			left.m00 * right.m03 + left.m01 * right.m13 + left.m02 * right.m23 + left.m03 * right.m33,
+			left.m10 * right.m00 + left.m11 * right.m10 + left.m12 * right.m20 + left.m13 * right.m30,
+			left.m10 * right.m01 + left.m11 * right.m11 + left.m12 * right.m21 + left.m13 * right.m31,
+			left.m10 * right.m02 + left.m11 * right.m12 + left.m12 * right.m22 + left.m13 * right.m32,
+			left.m10 * right.m03 + left.m11 * right.m13 + left.m12 * right.m23 + left.m13 * right.m33,
+			left.m20 * right.m00 + left.m21 * right.m10 + left.m22 * right.m20 + left.m23 * right.m30,
+			left.m20 * right.m01 + left.m21 * right.m11 + left.m22 * right.m21 + left.m23 * right.m31,
+			left.m20 * right.m02 + left.m21 * right.m12 + left.m22 * right.m22 + left.m23 * right.m32,
+			left.m20 * right.m03 + left.m21 * right.m13 + left.m22 * right.m23 + left.m23 * right.m33,
+			left.m30 * right.m00 + left.m31 * right.m10 + left.m32 * right.m20 + left.m33 * right.m30,
+			left.m30 * right.m01 + left.m31 * right.m11 + left.m32 * right.m21 + left.m33 * right.m31,
+			left.m30 * right.m02 + left.m31 * right.m12 + left.m32 * right.m22 + left.m33 * right.m32,
+			left.m30 * right.m03 + left.m31 * right.m13 + left.m32 * right.m23 + left.m33 * right.m33
+		);
 	}
 
 	constexpr Matrix4x4& operator+=(Matrix4x4& left, const Matrix4x4& right)
